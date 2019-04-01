9660e03a117a9d933b00f2de490097072ca728a4
Van Halbert <vhalbert@redhat.com>
2019-04-01 16:06:51 -0400

    Add initial file

---
+++ fuse_online_config.sh
@@ -0,0 +1,21 @@
+# Tag for release. Update this before running release.sh
+git_fuse_online_install="version.fuse.online"
+
+# Tags used for the productised images
+tag_server="version.tag.server"
+tag_ui="version.tag.ui"
+tag_meta="version.tag.meta"
+tag_s2i="version.tag.s2i"
+
+tag_upgrade="version.tag.upgrade"
+tag_operator="version.tag.operator"
+
+
+# Docker repository for productised images
+repository="repo.name"
+
+# Test:
+registry="registry.name"
+
+# Official:
+# registry="registry.access.redhat.com"
